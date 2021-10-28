1eca25549d99cdbda79bd6b2e118937b9a51bffa
Votals <vvotals@gmail.com>
2021-10-28 15:37:32 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+import java.lang.*;
+
+public class Main {
+
+    public static void main(String[] args) throws java.lang.Exception {
+
+        String s = "private String filterString(String code) {\n" +
+                "  // lets say code = \"some code //comment inside\"\n" +
+                "  // return the string \"some code\" (without the comment)\n}";
+
+
+
+        s = s.replaceAll("//.*?\n","\n");
+        System.out.println("s=" + s);
+    }
+}
